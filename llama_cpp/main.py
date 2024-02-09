21c16ef1ee8405c57de0aef55e4b8ea1fb4e4aa0
畑田祐陽 <yuhihatada@gmail.com>
2024-02-09 16:13:07 +0900

    feat: create api

--- llama_cpp/main.py
+++ llama_cpp/main.py
@@ -1,22 +1,28 @@
 import os
 from llama_cpp import Llama
 from dotenv import load_dotenv
+from flask import Flask, jsonify, request
 
 load_dotenv()
 model_name = os.getenv("MODEL_NAME")
 model_path = "./models/" + model_name
-
 model = Llama(model_path=model_path)
 
-prompt = """### Instruction: What is the height of Mount Fuji?
-### Response:"""
+def generate_response(model, user_message):
+    prompt = f"""#Instruction: {user_message} #Response:"""
+    max_tokens = 100
 
-# 推論の実行
-output = model(
-    prompt,
-    temperature=0.1,
-    stop=["Instruction:", "Input:", "Response:", "\n"],
-    echo=True,
-)
+    output = model(prompt, max_tokens=max_tokens, echo=True)
 
-print(output["choices"][0]["text"])
+    result = output["choices"][0]["text"].split("Response: ", 1)[-1]
+    return result
+
+app = Flask(__name__)
+@app.route('/chat', methods=['POST'])
+def chat():
+    json_data = request.json
+    result = generate_response(model, json_data['text'])
+    return jsonify({"result": result})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3001)
